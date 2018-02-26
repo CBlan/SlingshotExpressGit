d751c9520dd5668ffcade303ef54adc58df67496
Chris <845865182@auckland.natcoll.ac.nz>
2018-02-26 15:14:38 +1300

    Ticksin

---
+++ Assets/TurnOnTick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnTick : MonoBehaviour {
+
+    public GameObject colliders;
+    public GameObject tick;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!colliders.activeSelf)
+        {
+            tick.SetActive(true);
+        }
+	}
+}
